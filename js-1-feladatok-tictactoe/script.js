2ecaaae156cad8de2d75ee98c5ce1c8f6f551de8
Keltaisten <keltaisten@gmail.com>
2021-11-18 18:03:24 +0100

    updated css

--- js-1-feladatok-tictactoe/script.js
+++ js-1-feladatok-tictactoe/script.js
@@ -16,7 +16,7 @@ generateArea();                                                 /* meghívása a
 const gridNodelist = document.querySelectorAll(`.cells`);       /* nodelist létrehozása */
 const gridArray = Array.from(gridNodelist);                     /* abból egy lista amin lehet dolgozni */
 
-const markSomething = (item) => item.target.textContent = "X";  //kell a target is!
+const markSomething = (item) => item.target.textContent = "X";  //kell a target is! !!!
 
 const markSome = (item) => {
     if(firstPlayer === 1){
@@ -38,7 +38,7 @@ const arrayOfArray = [
     gridArray.slice(0,3),
     gridArray.slice(3,6),
     gridArray.slice(6,9),
-    gridArray.filter((_, index) => index%3 === 0),          //miért kell az alulvonás?
+    gridArray.filter((_, index) => index%3 === 0),          //miért kell az alulvonás? !!!
     gridArray.filter((_, index) => index%3 === 1),
     gridArray.filter((_, index) => index%3 === 2),
     gridArray.filter((_, index) => index%4 === 0),
@@ -49,7 +49,7 @@ const arrayOfArray = [
 //     if(
 //     arrayOfArray.some(
 //         (index) => 
-//             index.every((item) => item.textContent === "X") ||       //textcontent kell
+//             index.every((item) => item.textContent === "X") ||       //textcontent kell!!!
 //             index.every((item) => item.textContent === "O")
 //     )
 //     ){
@@ -59,19 +59,29 @@ const arrayOfArray = [
 
 function checkOfTheWinner () {
     if(
-    arrayOfArray.some(                                              //miért some miért nem foreach? 
+    arrayOfArray.some(                                              //miért some miért nem foreach? !!!
         (index) => 
-            index.every((item) => item.textContent === "X") ||       //textcontent kell
+            index.every((item) => item.textContent === "X") ||       //textcontent kell !!!
             index.every((item) => item.textContent === "O")
     )
     ){
-        won();
+        won(0);
+    } else if(gridArray.every((item) => item.textContent === "X" ||item.textContent === "O")){
+        won(1);
     }
 }
 
-const won = () => {
+const won = (index) => {
     const winner = document.querySelector(`.winner`);
     winner.style.display = `flex`;
     const backgr = document.querySelector('body');
-    backgr.style.backgroundColor = `rgb(100,100,100)`;
+    backgr.style.backgroundColor = `rgb(180,180,180)`;
+    const h2Value = document.querySelector(`.h2`);
+    const winnerXorO = document.querySelector(`#whichWin`);
+    if(index === 0){
+        firstPlayer === 1 ? winnerXorO.textContent = ` X ` : winnerXorO.textContent = ` O `;
+        firstPlayer === 0 ? winnerXorO.textContent = ` O ` : winnerXorO.textContent = ` X `;
+    }else{
+        h2Value.textContent = "It's a draw!";
+    }
 }
